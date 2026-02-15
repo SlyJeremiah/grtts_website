a7697b14c5a5f184b592a348d97a67f97b3599d5
unknown <shanyaslym19@gmail.com>
2026-02-16 01:00:58 +0200

    Add 'app' alias for Vercel WSGI

--- grtts_project/wsgi.py
+++ grtts_project/wsgi.py
@@ -1,16 +1,16 @@
 """
 WSGI config for grtts_project project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+It exposes the WSGI callable as a module-level variable named `application`.
+For Vercel, we also need to expose it as `app`.
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grtts_project.settings')
 
 application = get_wsgi_application()
+
+# Vercel looks for 'app' - so we create an alias
+app = application
