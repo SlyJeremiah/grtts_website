e0a8ffa9909f4fdbeb755b8ba4fcf537cc986a3e
SlyJeremiah <shanyaslym19@gmail.com>
2026-02-18 13:43:48 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,16 @@
-"""
-Blog URL Configuration
-"""
 from django.urls import path
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    # Blog post list (main blog page)
+    # Main blog listing
     path('', views.post_list, name='post_list'),
     
-    # Individual blog post detail
+    # Post detail
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     
-    # Posts by category
+    # Category and tag listings
     path('category/<slug:slug>/', views.category_list, name='category_list'),
-    
-    # Posts by tag
     path('tag/<slug:slug>/', views.tag_list, name='tag_list'),
-    
-    # IMPORTANT: No test-email URL here - that belongs in main app
 ]
